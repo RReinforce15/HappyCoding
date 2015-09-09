ff58e8df092483e2d7727f91ba2b6f9dc8595940
Reinforce <testmodetestmodetestmode2@mail.com>
2015-09-09 11:21:54 +0800

    Create output_text.c

---
+++ output_text.c
@@ -0,0 +1,9 @@
+#include "output_text.h"
+
+//Character output function
+void output_text() {
+	/* ↓↓Here,you can change whatever character you want↓↓ :) */
+	///printf(" \(^o^)/ ");
+	///printf("Q_Q ");
+	printf("哼");
+}
