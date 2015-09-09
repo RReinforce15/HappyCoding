4834d1b141ee7c982840f1c397f682148e459f98
Reinforce <testmodetestmodetestmode2@mail.com>
2015-09-09 11:21:08 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ *2015~9999 Reinforce15 Inc. No rights reserved~~~
+ *Read more? you can see the ReadMe file :) 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+//The maximum height of the triangle
+/*↓You can change whatever the maximum height of the triangle you want↓:)*/
+#define Level 6
+
+int main(void)
+{
+	/* You can change whatever character you want in output_text.c file :) */
+	print_triangle(Level);
+	eat_bobo(1);
+	//eat_bobo(2);
+
+	system("PAUSE");
+	return 0;
+}
