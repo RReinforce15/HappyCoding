166617e18c849954e1bfe9ed0e0c6c0c38fa14e8
Reinforce <testmodetestmodetestmode2@mail.com>
2015-09-09 11:21:35 +0800

    Create eat_bobo.c

---
+++ eat_bobo.c
@@ -0,0 +1,12 @@
+#include "eat_bobo.h"
+//I am not nuisance..WTF
+void eat_bobo(int x) {
+	//what you want? dochi?
+	if (x == 1) {
+		printf("吃大便啦 討厭鬼\n");
+	}
+	else {
+		printf("幹你娘 吃大便啦討厭鬼\n");
+		; 
+	}
+}
